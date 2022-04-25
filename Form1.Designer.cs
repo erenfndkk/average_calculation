aa353416222a01c2775973c802b2013684a430ed
Eren Fındık <90691429+erenfndkk@users.noreply.github.com>
2022-04-25 14:37:38 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace ortalama_hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1_vize = new System.Windows.Forms.Label();
+            this.label2_odev1 = new System.Windows.Forms.Label();
+            this.label3_odev2 = new System.Windows.Forms.Label();
+            this.label4_odev3 = new System.Windows.Forms.Label();
+            this.label5_final = new System.Windows.Forms.Label();
+            this.text_vize = new System.Windows.Forms.TextBox();
+            this.text_odev1 = new System.Windows.Forms.TextBox();
+            this.text_odev2 = new System.Windows.Forms.TextBox();
+            this.text_odev3 = new System.Windows.Forms.TextBox();
+            this.text_final = new System.Windows.Forms.TextBox();
+            this.ortalama_lbl = new System.Windows.Forms.Label();
+            this.button1_orthesaplama = new System.Windows.Forms.Button();
+            this.harfnotu_lbl = new System.Windows.Forms.Label();
+            this.lbl_ortalama = new System.Windows.Forms.Label();
+            this.lbl___harfnotu = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1_vize
+            // 
+            this.label1_vize.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label1_vize.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1_vize.Location = new System.Drawing.Point(12, 28);
+            this.label1_vize.Name = "label1_vize";
+            this.label1_vize.Size = new System.Drawing.Size(77, 27);
+            this.label1_vize.TabIndex = 0;
+            this.label1_vize.Text = "vize";
+            // 
+            // label2_odev1
+            // 
+            this.label2_odev1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label2_odev1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2_odev1.Location = new System.Drawing.Point(12, 67);
+            this.label2_odev1.Name = "label2_odev1";
+            this.label2_odev1.Size = new System.Drawing.Size(77, 27);
+            this.label2_odev1.TabIndex = 1;
+            this.label2_odev1.Text = "ödev 1";
+            // 
+            // label3_odev2
+            // 
+            this.label3_odev2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label3_odev2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3_odev2.Location = new System.Drawing.Point(12, 109);
+            this.label3_odev2.Name = "label3_odev2";
+            this.label3_odev2.Size = new System.Drawing.Size(77, 27);
+            this.label3_odev2.TabIndex = 2;
+            this.label3_odev2.Text = "ödev 2";
+            // 
+            // label4_odev3
+            // 
+            this.label4_odev3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label4_odev3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4_odev3.Location = new System.Drawing.Point(12, 153);
+            this.label4_odev3.Name = "label4_odev3";
+            this.label4_odev3.Size = new System.Drawing.Size(77, 27);
+            this.label4_odev3.TabIndex = 3;
+            this.label4_odev3.Text = "ödev 3";
+            // 
+            // label5_final
+            // 
+            this.label5_final.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label5_final.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5_final.Location = new System.Drawing.Point(214, 62);
+            this.label5_final.Name = "label5_final";
+            this.label5_final.Size = new System.Drawing.Size(77, 27);
+            this.label5_final.TabIndex = 4;
+            this.label5_final.Text = "final";
+            // 
+            // text_vize
+            // 
+            this.text_vize.Location = new System.Drawing.Point(127, 28);
+            this.text_vize.Name = "text_vize";
+            this.text_vize.Size = new System.Drawing.Size(66, 20);
+            this.text_vize.TabIndex = 5;
+            // 
+            // text_odev1
+            // 
+            this.text_odev1.Location = new System.Drawing.Point(127, 67);
+            this.text_odev1.Name = "text_odev1";
+            this.text_odev1.Size = new System.Drawing.Size(66, 20);
+            this.text_odev1.TabIndex = 6;
+            // 
+            // text_odev2
+            // 
+            this.text_odev2.Location = new System.Drawing.Point(127, 109);
+            this.text_odev2.Name = "text_odev2";
+            this.text_odev2.Size = new System.Drawing.Size(66, 20);
+            this.text_odev2.TabIndex = 7;
+            // 
+            // text_odev3
+            // 
+            this.text_odev3.Location = new System.Drawing.Point(127, 153);
+            this.text_odev3.Name = "text_odev3";
+            this.text_odev3.Size = new System.Drawing.Size(66, 20);
+            this.text_odev3.TabIndex = 8;
+            // 
+            // text_final
+            // 
+            this.text_final.Location = new System.Drawing.Point(315, 62);
+            this.text_final.Name = "text_final";
+            this.text_final.Size = new System.Drawing.Size(66, 20);
+            this.text_final.TabIndex = 9;
+            // 
+            // ortalama_lbl
+            // 
+            this.ortalama_lbl.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ortalama_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ortalama_lbl.Location = new System.Drawing.Point(214, 109);
+            this.ortalama_lbl.Name = "ortalama_lbl";
+            this.ortalama_lbl.Size = new System.Drawing.Size(77, 27);
+            this.ortalama_lbl.TabIndex = 10;
+            this.ortalama_lbl.Text = "ortalama";
+            // 
+            // button1_orthesaplama
+            // 
+            this.button1_orthesaplama.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1_orthesaplama.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1_orthesaplama.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1_orthesaplama.Location = new System.Drawing.Point(57, 204);
+            this.button1_orthesaplama.Name = "button1_orthesaplama";
+            this.button1_orthesaplama.Size = new System.Drawing.Size(93, 38);
+            this.button1_orthesaplama.TabIndex = 12;
+            this.button1_orthesaplama.Text = "hesapla";
+            this.button1_orthesaplama.UseVisualStyleBackColor = false;
+            this.button1_orthesaplama.Click += new System.EventHandler(this.button1_orthesaplama_Click);
+            // 
+            // harfnotu_lbl
+            // 
+            this.harfnotu_lbl.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.harfnotu_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.harfnotu_lbl.Location = new System.Drawing.Point(214, 152);
+            this.harfnotu_lbl.Name = "harfnotu_lbl";
+            this.harfnotu_lbl.Size = new System.Drawing.Size(77, 27);
+            this.harfnotu_lbl.TabIndex = 13;
+            this.harfnotu_lbl.Text = "harf notu";
+            // 
+            // lbl_ortalama
+            // 
+            this.lbl_ortalama.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lbl_ortalama.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_ortalama.Location = new System.Drawing.Point(304, 108);
+            this.lbl_ortalama.Name = "lbl_ortalama";
+            this.lbl_ortalama.Size = new System.Drawing.Size(77, 27);
+            this.lbl_ortalama.TabIndex = 14;
+            this.lbl_ortalama.Text = "0";
+            // 
+            // lbl___harfnotu
+            // 
+            this.lbl___harfnotu.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lbl___harfnotu.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl___harfnotu.Location = new System.Drawing.Point(304, 152);
+            this.lbl___harfnotu.Name = "lbl___harfnotu";
+            this.lbl___harfnotu.Size = new System.Drawing.Size(77, 27);
+            this.lbl___harfnotu.TabIndex = 15;
+            this.lbl___harfnotu.Text = "cc";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(436, 276);
+            this.Controls.Add(this.lbl___harfnotu);
+            this.Controls.Add(this.lbl_ortalama);
+            this.Controls.Add(this.harfnotu_lbl);
+            this.Controls.Add(this.button1_orthesaplama);
+            this.Controls.Add(this.ortalama_lbl);
+            this.Controls.Add(this.text_final);
+            this.Controls.Add(this.text_odev3);
+            this.Controls.Add(this.text_odev2);
+            this.Controls.Add(this.text_odev1);
+            this.Controls.Add(this.text_vize);
+            this.Controls.Add(this.label5_final);
+            this.Controls.Add(this.label4_odev3);
+            this.Controls.Add(this.label3_odev2);
+            this.Controls.Add(this.label2_odev1);
+            this.Controls.Add(this.label1_vize);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1_vize;
+        private System.Windows.Forms.Label label2_odev1;
+        private System.Windows.Forms.Label label3_odev2;
+        private System.Windows.Forms.Label label4_odev3;
+        private System.Windows.Forms.Label label5_final;
+        private System.Windows.Forms.TextBox text_vize;
+        private System.Windows.Forms.TextBox text_odev1;
+        private System.Windows.Forms.TextBox text_odev2;
+        private System.Windows.Forms.TextBox text_odev3;
+        private System.Windows.Forms.TextBox text_final;
+        private System.Windows.Forms.Label ortalama_lbl;
+        private System.Windows.Forms.Button button1_orthesaplama;
+        private System.Windows.Forms.Label harfnotu_lbl;
+        private System.Windows.Forms.Label lbl_ortalama;
+        private System.Windows.Forms.Label lbl___harfnotu;
+    }
+}
+
